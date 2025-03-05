9ac873e790a2031d17d1618187518dac004a7924
jagoum <jagoum24@gmail.com>
2025-03-05 06:15:48 +0100

    Added files to define traits

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use todo::structs::{done::Done, pending::Pending};
+use std::iter;
+
+use todo::{structs::{done::Done, pending::Pending}, todo_factory, ItemTypes};
 
 mod todo;
 
 fn main() {
     // println!("Hello, world!");
+
     let done = Done::new("Shopping");
+    let todo_item = match todo_factory(&done.get_status(), &done.get_title()) {
+        Ok(data) => {match data {
+        ItemTypes::Done(item   ) => {println!("It is a done item with title {}",item.get_title())},
+        ItemTypes::Pending(item)=> {println!("It is a pending item with title {}", item.get_title())},
+        }},
+        Err(e) => {eprintln!("Sorry could get item type");return;},
+    };
     println!("{}",done.get_title());
     println!("{}\n",done.get_status());
 
